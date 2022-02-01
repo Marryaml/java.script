8f25bfa6107a300201f817fad4c15a787c5fd25a
Marryaml <94197586+Marryaml@users.noreply.github.com>
2022-02-01 15:54:39 +0100

    Homework 1

---
+++ script1.js
@@ -0,0 +1,69 @@
+// Exercise1
+    let user = {
+        firstname: 'giorgi',
+        lastname: 'smith',
+        age: 25,
+        studentstatus: active
+    };
+    console.log(user.studentstatus);
+    
+// Exercise2
+    let numbers = [1, 2, 3, 4, 5, 6, 7,];
+
+    // for (let i=0; i<=7; i++){
+    //     console.log(numbers[i])
+    // }
+
+    let item =0;
+    while (item <numbers.length){
+        console.log(numbers.[item]);
+        item++;
+    }
+
+// // Exercise3
+    let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+    for (let i=0; i>5; i++){
+    console.log(numbers[i])
+    }
+
+
+//  Exercise4
+let users =  [
+    {username:'test1', status: false},
+    {username:'test2', status: false},
+    {username:'test3', status: true}
+];
+
+console.log(users.[2].status);
+
+//  Exercise5
+let user = {
+	name: 'giorgi',
+	age:  20,
+	studentstatus: active
+} 
+
+    if (user.age > 18) {
+        user.studentstatus = 'active';
+        console.log(hello);
+    }
+
+    else if (user.name= 'giorgi') {
+        console.log(hello giorgi);
+    }
+
+    else if (user.studentstatus= 'active'){
+        (user.age < 25 );
+        console.log(hello world);
+
+    } 
+     else{
+     console.log(Error);
+}
+
+//  Exercise6
+
+let array = [ watermelon, pear, 10, 45, 50, 'apple', 'ananas'] ;
+for (let i=0; i=String; i++ ){
+    console.log(array[i]);
+}
